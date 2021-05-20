d85d64b35f7a64c8f840ef72a4d35ee7bc7ac2e5
Daniel Svitan <dankosaurus9@gmail.com>
2021-05-20 17:44:17 +0200

    :sparkles: Adds more stages

--- src/main.py
+++ src/main.py
@@ -21,10 +21,10 @@ def main():
         # in case of user quits before text has finished drawing
         try:
             for _ in range(25):
-                number_spam_line = Text(screen, pygame.font.SysFont("Helvetica", 18), "    ", (20, y + 20))
+                number_spam_line = Text(screen, pygame.font.SysFont("Lucida Console", 18), "    ", (20, y + 20))
                 screen.add_component(number_spam_line)
 
-                for __ in range(100):
+                for __ in range(65):
                     number_spam_line.set_text(number_spam_line.get_text() + str(randint(0, 1)))
 
                 number_spam_line.draw()
@@ -48,37 +48,60 @@ def main():
         screen.add_component(Text(screen, pygame.font.SysFont("Helvetica", 36),
                                   "                       DO NOT CLOSE THIS WINDOW!!!", (20, 20), color=(255, 0, 0)))
 
-        def show_troll_message() -> None:
-            troll_message = Text(screen, pygame.font.SysFont("Helvetica", 26), "", (20, 75))
-            screen.add_component(troll_message)
+        def show_troll_messages() -> None:
+            def new_stage(percentages: dict, y_position: int) -> None:
+                troll_message = Text(screen, pygame.font.SysFont("Helvetica", 26), "", (20, 75 + y_position))
+                screen.add_component(troll_message)
 
-            # percentage : what message will be displayed under that percentage
-            percentages = {
-                100: "Sending browser history to: MOM, GRANDMA... ",
-                75: "Downloading browser history... ",
-                50: "Stealing credit card numbers... ",
-                25: "Stealing passwords... ",
-            }
+                # in case of user quits before text has finished drawing
+                try:
+                    for current_percentage in range(101):
+                        message = ""
 
-            # in case of user quits before text has finished drawing
-            try:
-                for i in range(101):
-                    message = ""
+                        for percentage in percentages:
+                            if current_percentage <= percentage:
+                                message = percentages[percentage]
 
-                    for percentage in percentages:
-                        if i < percentage:
-                            message = percentages[percentage]
+                        troll_message.set_text(f"{message}{current_percentage}% complete")
 
-                    troll_message.set_text(f"{message}{i}% complete")
+                        screen.clear()
+                        screen.render()
+                        sleep(randint(10, 90) / 100)
 
-                    screen.clear()
-                    screen.render()
-                    sleep(0.1)
+                except pygame.error:
+                    pass
 
-            except pygame.error:
-                pass
+            for i in range(3):
+                if i == 0:
+                    new_stage({
+                        100: "Publishing your credit card information... ",
+                        75: "Retrieving credit card password... ",
+                        50: "Saving credit card information... ",
+                        25: "Retrieving credit card information... ",
+                    }, 30 * i)
 
-        screen.add_thread(show_troll_message, 0)
+                elif i == 1:
+                    new_stage({
+                        100: "Sending browser history to: MOM, GRANDMA... ",
+                        75: "Formatting browser history... ",
+                        50: "Reading browser history... ",
+                        25: "Downloading browser history... ",
+                    }, 30 * i)
+
+                elif i == 2:
+                    new_stage({
+                        100: "Changing your steam account password... ",
+                        75: "Cracking steam account... ",
+                        50: "Downloading games passwords... ",
+                        25: "Downloading games data... ",
+                    }, 30 * i)
+
+            cya_message = Text(screen, pygame.font.SysFont("Helvetica", 26), "Thanks and have a nice day!", (20, 165))
+            screen.add_component(cya_message)
+            screen.clear()
+            screen.render()
+
+        screen.add_thread(show_troll_messages, 0)
         screen.set_exit_callback(on_exit)
 
         screen.render()
